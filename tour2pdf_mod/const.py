caf75353de30b36755a48cb57ba7a16509cb11e6
Sven Anders <sven@anders-hamburg.de>
2024-07-08 15:29:25 +0200

    Version 0.10 Mehrere Preise mit Komma. Link hinter qrcode zum Klicken

--- tour2pdf_mod/const.py
+++ tour2pdf_mod/const.py
@@ -8,6 +8,6 @@ class AppConst:
     RADTOUR_URL = "https://touren-termine.adfc.de/suche?eventType=Radtour&unitKey=158"
     # API_BASE_URL = "https://dev-api-touren-termine.adfc.de/api"
     API_BASE_URL = "https://api-touren-termine.adfc.de/api"
-    VERSION = "0.9"
+    VERSION = "0.10"
     TOUR_URL_PREFIX = "https://touren-termine.adfc.de/radveranstaltung/"
     SHOW_API_LINK = False
