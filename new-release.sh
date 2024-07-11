b272bb631ced62ebc196209646ab6e9f1f961ad8
Sven Anders <sanders@scaleuptech.com>
2024-07-11 09:27:52 +0200

    new Release fix

--- new-release.sh
+++ new-release.sh
@@ -5,7 +5,7 @@ if [ "$VERSION" == "" ]; then
     echo Bitte version angeben
     exit
 fi
-sed -ie "s/VERSION = \".*\"\$/VERSION = \"$VERSION\"/" tour2pdf_mod/const.py
+sed -i -e "s/VERSION = \".*\"\$/VERSION = \"$VERSION\"/" tour2pdf_mod/const.py
 git commit -m "Neue Version $VERSION" tour2pdf_mod/const.py
 git tag v$VERSION
 git push origin v$VERSION
