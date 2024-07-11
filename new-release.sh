c34a7966b70624aa736ec62648f82d1db53ad387
Sven Anders <sanders@scaleuptech.com>
2024-07-11 07:36:15 +0200

    Script für das bauen eines neuen Releases

--- new-release.sh
+++ new-release.sh
@@ -5,7 +5,7 @@ if [ "$VERSION" == "" ]; then
     echo Bitte version angeben
     exit
 fi
-sed -e "s/VERSION = \".*\"\$/VERSION = \"$VERSION\"/" tour2pdf_mod/const.py
+sed -ie "s/VERSION = \".*\"\$/VERSION = \"$VERSION\"/" tour2pdf_mod/const.py
 git commit -m "Neue Version $VERSION" tour2pdf_mod/const.py
 git tag v$VERSION
 git push origin v$VERSION
