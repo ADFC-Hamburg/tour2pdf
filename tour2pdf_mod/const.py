fde3d0d0232d37000eeeb347b6088f24d64ab2c7
Sven Anders <sanders@scaleuptech.com>
2024-07-11 07:32:38 +0200

    Neue Version 0.14

--- tour2pdf_mod/const.py
+++ tour2pdf_mod/const.py
@@ -8,6 +8,6 @@ class AppConst:
     RADTOUR_URL = "https://touren-termine.adfc.de/suche?eventType=Radtour&unitKey=158"
     # API_BASE_URL = "https://dev-api-touren-termine.adfc.de/api"
     API_BASE_URL = "https://api-touren-termine.adfc.de/api"
-    VERSION = "0.12"
+    VERSION = "0.14"
     TOUR_URL_PREFIX = "https://touren-termine.adfc.de/radveranstaltung/"
     SHOW_API_LINK = False
